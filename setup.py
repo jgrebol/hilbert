45142a856dcdddeb1210bc7b0125a393b6a9bb15
Joan Grebol <116028852+jgrebol@users.noreply.github.com>
2024-03-05 17:29:37 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='hilbert',
+   version='0.1',
+   description='A Python package to compute molecular properties and electronic structure indicators from PySCF calculations.',
+   url='https:/github.com/jgrebol/ESIpy',
+   author='Joan Grebol-Tomas, Pedro Salvador, Eduard Matito',
+   author_mail='joangrebol@gmail.com, psalse@gmail.com',
+   packages=find_packages(),
+)
+
+
